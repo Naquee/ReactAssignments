2f4cdf6c075e67f1592c8cd930b853242e036754
Naquee <naqueealam99@gmail.com>
2022-05-19 00:14:41 +0530

    a

---
+++ src/components/Hello.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export const Hello = () => {
+  return (
+    <div>
+        <h1>Oprating system</h1>
+        <ul>
+            <li>Andriod</li>
+            <li>Blackburry</li>
+            <li>iPhone</li>
+            <li>windows phone</li>
+        </ul>
+
+        <h1>Mobile Menufactures</h1>
+        <ul>
+            <li>samsung</li>
+            <li>htc</li>
+            <li>micromax</li>
+            <li>apple</li>
+        </ul>
+    </div>
+  )
+}
